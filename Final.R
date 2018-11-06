0f70fe659b82a6a9b1f4a64caa53ed9771c5699c
Henryxie1003 <yuhaoxie@buffalo.edu>
2018-11-06 15:33:01 -0500

    11062018

--- Final.R
+++ Final.R
@@ -48,21 +48,38 @@ task <- toJSON(task, auto_unbox=TRUE)
 token <- paste("Bearer", fromJSON(token_response)$token)
 response <- POST("https://lpdaacsvc.cr.usgs.gov/appeears/api/task", body = task, encode = "json", 
                  add_headers(Authorization = token, "Content-Type" = "application/json"))
-
 task_response <- prettify(toJSON(content(response), auto_unbox = TRUE))
 task_response
 
 #retrieve the task detail
 token <- paste("Bearer", fromJSON(token_response)$token)
-task_id <- "c94c5a01-72d4-4848-acc2-5a1a93c04051"
+task_id <- fromJSON(task_response)$task_id
 response <- GET(paste("https://lpdaacsvc.cr.usgs.gov/appeears/api/task/", task_id, sep = ""), add_headers(Authorization = token))
 task_response <- prettify(toJSON(content(response), auto_unbox = TRUE))
 task_response
 
 #bundle API
 token <- paste("Bearer", fromJSON(token_response)$token)
-task_id <- "4b3593ba-a734-4e87-a747-e4b487cfae4d"
 response <- GET(paste("https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/", task_id, sep = ""), add_headers(Authorization = token))
 bundle_response <- prettify(toJSON(content(response), auto_unbox = TRUE))
 bundle_response
 
+#Download file
+
+#get csv file id and name
+file_list<-fromJSON(bundle_response)$files
+file_id<-file_list$file_id[file_list$file_type=="csv"]
+file_name<-file_list$file_name[file_list$file_id==file_id]
+
+# create a destination directory to store the file in
+dest_dir <- getwd()
+filepath <- paste(dest_dir, file_name, sep = '/')
+suppressWarnings(dir.create(dirname(filepath)))
+
+# write the file to disk using the destination directory and file name 
+response <- GET(paste("https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/", task_id, '/', file_id, sep = ""),
+                write_disk(filepath, overwrite = TRUE), progress(), add_headers(Authorization = token))
+
+#read and view data
+data<-read.csv(file_name)
+View(data)
